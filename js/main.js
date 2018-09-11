b976f00f9fcd1ea678e650bfd4a86e7706a10c9e
praveen-me <praveenkrsaini0@gmail.com>
2018-09-12 01:40:58 +0530

    Complete functionality of Setting time and greeting user and setting user for first time

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
   (localStorage.getItem('todoTasks'));
 
   // function for Displaying to do list
-  let display = function(array, elm) {
+  let displayTasks = function(array, elm) {
     // Put content through innerHTML by innerHTML and map the array
     elm.innerHTML = array.map( (item, i) => {
       // Return the Elemnt String
@@ -41,13 +41,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Check all checkbox if it has true class then confirm done
     checkBoxes.forEach(checkBox => {
-      if(checkBox.classList.contains(true)) {
-        checkBox.setAttribute('checked', '');
-        // checkBox.nextElementSibling.style.textDecoration = 'line-through';
+      if(checkBox.classList.contains(true)) { // if it has true class
+        checkBox.setAttribute('checked', ''); // set checked attribute
         checkBox.nextElementSibling.style.cssText = 
-        "text-decoration : line-through; font-weight : normal";
+        "text-decoration : line-through; font-weight : normal"; // set some styles
       } else { // else it has false then set to default
-        // checkBox.nextElementSibling.style.textDecoration = 'none';
         checkBox.nextElementSibling.style.cssText = 
         "text-decoration : none; font-weight : bold";
       }
@@ -57,7 +55,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Check if something presents in local Storage
   if(parsedArray) {
     tasks = parsedArray;
-    display(tasks, todoListElement); 
+    displayTasks(tasks, todoListElement); 
   }
 
   //function to set item to local storage
@@ -83,7 +81,7 @@ document.addEventListener('DOMContentLoaded', function() {
     setItemsToLocalStorage();
 
     // Immediately Display the List
-      display(tasks, todoListElement);
+    displayTasks(tasks, todoListElement);
 
     //reset the value of task-value to empty string
     document.getElementById('task-value').value = '';
@@ -106,40 +104,128 @@ document.addEventListener('DOMContentLoaded', function() {
       setItemsToLocalStorage();
 
       // Display the list
-      display(tasks, todoListElement);
+      displayTasks(tasks, todoListElement);
     } else {
       // Check if checkbox is checked
       if(e.target.checked === true) {
         // set done to true
         tasks[id].done = true;
   
-        // 3 - Append the current tasks in the local storage
+        // Append the current tasks in the local storage
         setItemsToLocalStorage();
   
-        // display the list
-        display(tasks, todoListElement);      
+        // displayTasksTasks the list
+        displayTasks(tasks, todoListElement);      
       } else { // else set done to false it it checked again
         tasks[id].done = false;
 
         // set the all list to local storage
         setItemsToLocalStorage();
 
-        // display the list
-        display(tasks, todoListElement);
+        // displayTasksTasks the list
+        displayTasks(tasks, todoListElement);
       }
+    }
+  }
+
+  // Function for setDate
+  let setTime = function() {
+    // store date
+    let now = new Date();
+    // store hour element
+    let hour = document.getElementById('hour');
+    // store minute elementy
+    let minutes = document.getElementById('minutes');
+
+    // Update innerHTML of hour to current Hours
+    hour.innerHTML = now.getHours();
+    // Update innerHTML of minutes to current minutes
+    minutes.innerHTML = now.getMinutes();
+
+  }
+
+  // First Time Setup
+  // store first time block
+  let firstTimeBlock = document.querySelector('.first_time_block');
+
+  // store greet user block
+  let greet_user_block = document.querySelector('.greet_user_block');
+
+  // Set username value for first time
+  let setUserNameValues = function(e) {
+    // select userName value
+    let userNameValue = document.getElementById('user_name').value;
+
+    // Check wheather the keypress is 'Enter' key for setting userName value
+    if(e.keyCode === 13) {
+      // if it is then set userName value in localstorage
+      JSON.stringify(localStorage.setItem('userName', userNameValue));
+      //hide first time block
+      firstTimeBlock.style.display = 'none';
+      //show greet user block
+      greet_user_block.style.display = 'block';
+    }
+  }
+
+  // Check wheather the userName is stored or not
+  let checkUserNameValue = function() {
+    // check wheather localStorage has userName or not
+    if(localStorage.getItem('userName') === null) {
+      //is it is then display first time block
+      firstTimeBlock.style.display = 'block';
+      // set an event listner for taking userName and set it
+      document.body.addEventListener('keypress', setUserNameValues);
+    } else { // if it's not then display a greet message
+      // display greet user block
+      greet_user_block.style.display = 'block';
+
+      // Select Greet user
+      let greet_user = document.querySelector('.greet_user');
+      // Retrive userName value from local storage and set it to greet user element
+      greet_user.innerHTML = localStorage.getItem('userName');;
+    }
+  }
+
+  // Select Greet msg block
+  let greet_msg = document.querySelector('.greet_msg');
+
+  // set greetmsg function 
+  // @params : element(elm)
+  let setGreet = function(elm) {
+    let now = new Date();
+    // take hours
+    let hours = now.getHours();
+    //set greet msg according to hours
+    if(hours >= 4 && hour <= 12) {
+      elm.innerHTML =  'Good Morning';
+    } else if(hours >= 13 && hour <= 16) {
+      elm.innerHTML = 'Good AfterNoon';
+    } else if(hours >= 17 && hour <= 20) {
+      elm.innerHTML = 'Good Evening';
+    } else {
+      elm.innerHTML = 'Good Night';
     }
   }
 
   // Initialized function
   function init() {  
     // Display tasks on page loads
-    display(tasks, todoListElement);
+    displayTasks(tasks, todoListElement);
 
     // add a eventlistener to add task
     addTaskBtn.addEventListener('click',  addTask);
 
     //Add event listner on ul to deleteBooks and checked for done task
     todoListElement.addEventListener('click', deleteAndDoneTask);
+
+    //recall setDate function after 1s;
+    setInterval(setTime, 1000);
+    
+    // call function for checkUserName available in local Storage or not
+    checkUserNameValue();
+
+    // Set Greet According to Hour
+    setGreet(greet_msg);
   }
 
   init();                         
